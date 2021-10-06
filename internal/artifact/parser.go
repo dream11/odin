9365079d5d41fa01ab772127706e8b14327c6614
Harshit Sharma <sharma1612harshit@gmail.com>
2021-10-06 16:35:49 +0530

    internal module for artifact file parsing

---
+++ internal/artifact/parser.go
@@ -0,0 +1,37 @@
+package artifact
+
+import (
+	"io/ioutil"
+	"gopkg.in/yaml.v3"
+)
+
+type flavour struct {
+	Name       string               `yaml:"name"`
+	Version    map[string]string    `yaml:"version"`
+}
+
+type steps struct {
+	Pre      []string    `yaml:"pre"`
+	Build    []string    `yaml:"build"`
+	Post     []string    `yaml:"post"`
+}
+
+type Artifact struct {
+	Flavour         flavour    `yaml:"flavour"`
+	BuildFile       string     `yaml:"buildFile"`
+	Steps           steps      `yaml:"steps"`
+	ArtifactPath    string     `yaml:"artifactPath"`
+}
+
+func ParseFile(filePath string) (Artifact, error) {
+	var artifact Artifact
+
+	yFile, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return artifact, err
+	}
+
+	err = yaml.Unmarshal(yFile, &artifact)
+
+	return artifact, err
+}
