2301cccdf027fc06dafae33029f2c2e748c35305
Harshit Sharma <sharma1612harshit@gmail.com>
2021-10-07 00:32:33 +0530

    create file package

---
+++ pkg/file/file.go
@@ -0,0 +1,13 @@
+package file
+
+import (
+	"io/fs"
+	"os"
+)
+
+func Write(filePath, data string, permission fs.FileMode) error {
+	byteData := []byte(data)
+	err := os.WriteFile(filePath, byteData, permission)
+	
+	return err
+}
