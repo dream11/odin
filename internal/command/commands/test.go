6c96187f7caa0293f45b8af880134cd0dc3248a3
Harshit Sharma <sharma1612harshit@gmail.com>
2021-10-06 22:46:51 +0530

    add description

--- internal/command/commands/test.go
+++ internal/command/commands/test.go
@@ -4,17 +4,25 @@ import (
 	"log"
 )
 
+// --------------------------------------------------------
+// Test Command
+// --------------------------------------------------------
 type Test struct {}
 
+// Run implements the actual functionality of the command
+// and return exit codes based on success/failure of tasks performed
 func (t *Test) Run(args []string) int {
 	log.Println("test run")
 	return 0
 }
 
+// Help should return an explanatory string, 
+// that can explain the command
 func (t *Test) Help() string {
 	return "command help"
 }
 
+// Synopsis should return a breif helper text for the command
 func (t *Test) Synopsis() string {
 	return "command synopsis"
 }
