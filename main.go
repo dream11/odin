26a39bf363a200df9c3ce8b9b36b41dc9097847f
Harshit Sharma <sharma1612harshit@gmail.com>
2021-10-06 18:14:27 +0530

    parse property file

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"os"
-	"log"
 	"path"
+	"log"
 
 	"github.com/dream11/d11-cli/internal/artifact"
+	"github.com/dream11/d11-cli/internal/artifact/javaMaven"
+	// "github.com/dream11/d11-cli/pkg/shell"
 )
 
 const (
@@ -15,13 +18,26 @@ const (
 func main() {
 	componentDir := os.Args[1]
 	artifactFilePath := path.Join(componentDir, artifactFileName)
+	log.Println("Reading", artifactFilePath)
 
 	artifact, err := artifact.ParseFile(artifactFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	propertyFilePath := path.Join(componentDir, artifact.PropertyFile)
+	log.Println("Reading", propertyFilePath)
 
+	properties, err := javaMaven.ParseFile(propertyFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(artifact)
-	log.Println("Generating artifact at:", artifact.ArtifactPath, "using:", artifact.BuildFile)
+	artifactPath := path.Join(componentDir, properties.Properties.ArtifactPath)
+	log.Println("An artifact will be generated at:", artifactPath)
+
+	artifactName := fmt.Sprintf("%s-%s", properties.Name, properties.Version)
+	log.Println("Artifact name:", artifactName)
+
+	// shell.Exec(fmt.Sprintf("cd %s && %s", componentDir, artifact.Steps.Build[0]))
 }
