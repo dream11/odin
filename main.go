80a72631a1d9ce3c0e914a50ef37821cdc655868
Harshit Sharma <sharma1612harshit@gmail.com>
2021-10-06 22:22:36 +0530

    import commands from catalog

--- main.go
+++ main.go
@@ -6,16 +6,31 @@ import (
 	"path"
 	"log"
 
+	"github.com/dream11/d11-cli/internal/command"
 	"github.com/dream11/d11-cli/internal/artifact"
+	"github.com/mitchellh/cli"
 	"github.com/dream11/d11-cli/internal/artifact/javaMaven"
 	"github.com/dream11/d11-cli/pkg/shell"
 )
 
 const (
+	appName = "d11-cli"
+	appVersion = "1.0.0-beta"
 	artifactFileName = "artifact.yml"
 )
 
 func main() {
+	c := cli.NewCLI(appName, appVersion)
+	c.Args = os.Args[1:]
+	c.Commands = command.CommandCatalog()
+
+	exitStatus, err := c.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	os.Exit(exitStatus)
+
 	componentDir := os.Args[1]
 	artifactFilePath := path.Join(componentDir, artifactFileName)
 	log.Println("Reading", artifactFilePath)
