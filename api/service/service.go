eff875394fd6943e9136ce44ae1ea38badf89a51
Harshit Sharma <sharma1612harshit@gmail.com>
2021-10-14 15:35:14 +0530

    initiate api structures for profile+service+component

---
+++ api/service/service.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"fmt"
+)
+
+type Component struct {
+	Name       string    `yaml:"name" json:"name"`
+	Version    string    `yaml:"version" json:"version"`
+}
+
+type Service struct {
+	Name          string         `yaml:"name" json:"name"`
+	Version       string         `yaml:"version" json:"version"`
+	Components    []Component    `yaml:"components" json:"components"`
+}
+
+type Services []Service
+
+func (s *Services) GetService(name string) (Service, error) {
+	for _, service := range *s {
+		if service.Name == name {
+			return service, nil
+		}
+	}
+
+	return Service{}, fmt.Errorf("%s does not exists", name)
+}
