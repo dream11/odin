2e04e2b78e0224857b73baebf9896b053353ea64
Harshit Sharma <sharma1612harshit@gmail.com>
2021-10-20 15:35:45 +0530

    move initialization to application package

--- d11cli/app.go
+++ d11cli/app.go
@@ -1,5 +1,9 @@
 package d11cli
 
+import (
+	"github.com/dream11/d11-cli/internal/logger"
+)
+
 type application struct {
 	Name       string
 	Version    string
@@ -8,4 +12,9 @@ type application struct {
 var App application = application{
 	Name: "d11-cli",
 	Version: "1.0.0-beta",
+}
+
+// initiate logger on app initialisation
+func init() {
+	logger.HandleLogging()
 }
--- main.go
+++ main.go
@@ -5,19 +5,10 @@ import (
 
 	"github.com/dream11/d11-cli/d11cli"
 	"github.com/dream11/d11-cli/internal/cli"
-	"github.com/dream11/d11-cli/internal/logger"
 	"github.com/brownhash/golog"
 )
 
 func main() {
-	// handle logging format and levels
-	logger.HandleLogging()
-	// create/check workdir
-	err := d11cli.WorkDir.Create()
-	if err != nil {
-		golog.Error(err)
-	}
-
 	c := cli.Cli(d11cli.App.Name, d11cli.App.Version)
 	exitStatus, err := c.Run()
 	if err != nil {
