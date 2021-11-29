42173ba89fae007e3b60183a9ee9c8d9699eab09
Harshit Sharma <sharma1612harshit@gmail.com>
2021-11-29 19:44:17 +0530

    feat: list service structure

---
+++ api/service/service.go
@@ -0,0 +1,15 @@
+package service
+
+// Service structure
+type Service struct {
+	Name string `yaml:"name,omitempty" json:"name,omitempty"`
+	Version string `yaml:"version,omitempty" json:"version,omitempty"`
+	Team []string `yaml:"team,omitempty" json:"team,omitempty"`
+	Description string `yaml:"description,omitempty" json:"description,omitempty"`
+	Mature bool `yaml:"isMature,omitempty" json:"isMature,omitempty"`
+}
+
+// ListResponse structure
+type ListResponse struct {
+	Response []Service `yaml:"resp,omitempty" json:"resp,omitempty"`
+}
