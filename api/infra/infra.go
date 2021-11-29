5e64ffc6bd05fa20fccb94e42d040e8d671b4763
Harshit Sharma <sharma1612harshit@gmail.com>
2021-11-29 20:56:43 +0530

    fix: response structure for api support

--- api/infra/infra.go
+++ api/infra/infra.go
@@ -2,15 +2,20 @@ package infra
 
 // Infra structure
 type Infra struct {
-	Name         string `yaml:"name,omitempty" json:"name,omitempty"`
 	Team         string `yaml:"team,omitempty" json:"team,omitempty"`
-	Reason       string `yaml:"reason,omitempty" json:"reason,omitempty"`
+	Purpose      string `yaml:"purpose,omitempty" json:"purpose,omitempty"`
 	Env          string `yaml:"env_type,omitempty" json:"env_type,omitempty"`
 	State        string `yaml:"state,omitempty" json:"state,omitempty"`
 	DeletionTime string `yaml:"deletion_time,omitempty" json:"deletion_time,omitempty"`
+	Account      string `yaml:"provider_account,omitempty" json:"provider_account,omitempty"`
+}
+
+// CreationName structure
+type CreationName struct {
+	Name string `yaml:"infra_name,omitempty" json:"infra_name,omitempty"`
 }
 
 // CreationResponse structure
 type CreationResponse struct {
-	InfraName string `yaml:"infra_name,omitempty" json:"infra_name,omitempty"`
+	Response CreationName `yaml:"resp,omitempty" json:"resp,omitempty"`
 }
