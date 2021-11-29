b21aa74500c12fcdd11e7a55b8db415649e65626
Harshit Sharma <sharma1612harshit@gmail.com>
2021-11-29 22:25:32 +0530

    fix: explain infra

--- api/infra/infra.go
+++ api/infra/infra.go
@@ -9,6 +9,15 @@ type Infra struct {
 	State        string `yaml:"state,omitempty" json:"state,omitempty"`
 	DeletionTime string `yaml:"deletion_time,omitempty" json:"deletion_time,omitempty"`
 	Account      string `yaml:"provider_account,omitempty" json:"provider_account,omitempty"`
+	CreatedBy    string `yaml:"created_by,omitempty" json:"created_by,omitempty"`
+	UpdatedBy    string `yaml:"updated_by,omitempty" json:"updated_by,omitempty"`
+	CreatedAt    string `yaml:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt    string `yaml:"updated_at,omitempty" json:"updated_at,omitempty"`
+	Config       interface{} `yaml:"config,omitempty" json:"config,omitempty"`
+	MetaInfo     interface{} `yaml:"meta_info,omitempty" json:"meta_info,omitempty"`
+	Cost         string `yaml:"cost,omitempty" json:"cost,omitempty"`
+	Organization string `yaml:"organization,omitempty" json:"organization,omitempty"`
+	Services     []string `yaml:"services,omitempty" json:"services,omitempty"`
 }
 
 // CreationName structure
