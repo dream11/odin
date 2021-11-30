1bddc884ccacb2cc97a69ed0e03ea457f1f2f5a3
Harshit Sharma <sharma1612harshit@gmail.com>
2021-11-30 15:27:38 +0530

    feat: component structure

--- api/component/component.go
+++ api/component/component.go
@@ -12,3 +12,23 @@ type Component struct {
 	Type     string   `yaml:"type,omitempty" json:"type,omitempty"`
 	Artifact Artifact `yaml:"artifact,omitempty" json:"artifact,omitempty"`
 }
+
+// Type structure
+type Type struct {
+	Name        string      `yaml:"name,omitempty" json:"name,omitempty"`
+	CreatedBy   string      `yaml:"created_by,omitempty" json:"created_by,omitempty"`
+	UpdatedBy   string      `yaml:"updated_by,omitempty" json:"updated_by,omitempty"`
+	CreatedAt   string      `yaml:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt   string      `yaml:"updated_at,omitempty" json:"updated_at,omitempty"`
+	Config      interface{} `yaml:"config,omitempty" json:"config,omitempty"`
+	Deployment  interface{} `yaml:"deployment,omitempty" json:"deployment,omitempty"`
+	Scaling     interface{} `yaml:"scaling,omitempty" json:"scaling,omitempty"`
+	Discovery   interface{} `yaml:"discovery,omitempty" json:"discovery,omitempty"`
+	MetaInfo    interface{} `yaml:"meta_info,omitempty" json:"meta_info,omitempty"`
+	EnvBehavior interface{} `yaml:"env_specific_behavior,omitempty" json:"env_specific_behavior,omitempty"`
+}
+
+// ListTypeResponse structure
+type ListTypeResponse struct {
+	Response []Type `yaml:"resp,omitempty" json:"resp,omitempty"`
+}
