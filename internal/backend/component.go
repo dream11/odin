4749756fd2c853ac0a3c37319d8485642b642378
Harshit Sharma <sharma1612harshit@gmail.com>
2021-11-30 15:27:53 +0530

    feat: componet list api

--- internal/backend/component.go
+++ internal/backend/component.go
@@ -1,14 +1,23 @@
 package backend
 
+import (
+	"encoding/json"
+
+	"github.com/dream11/odin/api/component"
+)
+
 // Component entity
 type Component struct{}
 
 // ListComponents : list all available component types
-func (c *Component) ListComponents() {
+func (c *Component) ListComponents() ([]component.Type, error) {
 	client := newApiClient()
 
 	response := client.action("componenttypes", "GET", nil)
 	response.Process(true) // process response and exit if error
 
-	// TODO: parse response.Body into required structure and return
+	var componentTypeResponse component.ListTypeResponse
+	err := json.Unmarshal(response.Body, &componentTypeResponse)
+
+	return componentTypeResponse.Response, err
 }
