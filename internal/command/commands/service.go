77fbcee55d1c3ca01bdbd7b0f533a2cf6635b119
Kishan <balkishan@dream11.com>
2022-05-30 17:37:38 +0530

    removed force and rebuild flags for service deploy

--- internal/command/commands/service.go
+++ internal/command/commands/service.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/dream11/odin/api/service"
 	"github.com/dream11/odin/internal/backend"
 	"github.com/dream11/odin/pkg/file"
 	"github.com/dream11/odin/pkg/table"
@@ -26,7 +27,6 @@ func (s *Service) Run(args []string) int {
 	filePath := flagSet.String("file", "service.json", "file to read service config")
 	serviceName := flagSet.String("name", "", "name of service to be used")
 	serviceVersion := flagSet.String("version", "", "version of service to be used")
-	force := flagSet.Bool("force", false, "forcefully deploy the new version of the service")
 	envName := flagSet.String("env", "", "name of environment to deploy the service in")
 	teamName := flagSet.String("team", "", "name of user's team")
 	rebuild := flagSet.Bool("rebuild", false, "rebuild executor for creating images or deploying services")
@@ -178,8 +178,51 @@ func (s *Service) Run(args []string) int {
 	if s.Deploy {
 		emptyParameters := emptyParameters(map[string]string{"--name": *serviceName, "--version": *serviceVersion, "--env": *envName})
 		if len(emptyParameters) == 0 {
-			s.Logger.Info("Initiating service deployment: " + *serviceName + "@" + *serviceVersion + " in " + *envName)
-			serviceClient.DeployServiceStream(*serviceName, *serviceVersion, *envName, *configStoreNamespace, *force, *rebuild)
+			envServices, err := envClient.DescribeEnv(*envName, "", "")
+
+			if err != nil {
+				s.Logger.Error(err.Error())
+				return 1
+			}
+
+			envService := service.Service{}
+
+			rebuildService := false
+			forceService := false
+
+			for _, curService := range envServices.Services {
+				if curService.Name == *serviceName && curService.Version == *serviceVersion {
+					rebuildService = true
+					envService = curService
+					break
+				}
+				if curService.Name == *serviceName && curService.Version != *serviceVersion {
+					forceService = true
+					envService = curService
+					break
+				}
+			}
+
+			if forceService {
+				s.Logger.Info(fmt.Sprintf("service: %s already exists in the env with different version: %s", *serviceName, envService.Version))
+				s.Logger.Output("Press [Y] to force deploy service or press [n] to skip service deploy.\n")
+				message := fmt.Sprintf("Update version of Service %s : %s -> %s[Y/n]: ", *serviceName, envService.Version, *serviceVersion)
+
+				allowedInputs := map[string]struct{}{"Y": {}, "n": {}}
+				val, err := s.Input.AskWithConstraints(message, allowedInputs)
+
+				if err != nil {
+					s.Logger.Error(err.Error())
+					return 1
+				}
+
+				if val != "Y" {
+					s.Logger.Info("Skipping force service deploy")
+					return 0
+				}
+			}
+
+			s.deployService(*serviceName, *serviceVersion, *envName, *configStoreNamespace, forceService, rebuildService)
 
 			return 0
 		}
@@ -250,6 +293,12 @@ func (s *Service) Run(args []string) int {
 
 	s.Logger.Error("Not a valid command")
 	return 127
+}
+
+func (s *Service) deployService(serviceName, serviceVersion, envName, configStoreNamespace string, force, rebuild bool) {
+	s.Logger.Debug(fmt.Sprintf("%s: %s : %s: %s: %t: %t", serviceName, serviceVersion, envName, configStoreNamespace, force, rebuild))
+	s.Logger.Info("Initiating service deployment: " + serviceName + "@" + serviceVersion + " in " + envName)
+	serviceClient.DeployServiceStream(serviceName, serviceVersion, envName, configStoreNamespace, force, rebuild)
 }
 
 // Help : returns an explanatory string
