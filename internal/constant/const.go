f7fba1728aebbecef5e5659fd646e898be8026d1
sarveshdream11 <91313228+sarveshdream11@users.noreply.github.com>
2022-11-18 15:50:06 +0530

    Dx 686 proper error message (#154)
    * added new command for updating an env (#145)
    
    * added new command for updating an env
    
    * added helper for env update
    
    Co-authored-by: Rahul Rayal <rahul.rayal@D11-Rahul.Rayal>
    
    * adding proper error messages
    
    Co-authored-by: rahulrayal <110025849+rahulrayal@users.noreply.github.com>
    Co-authored-by: Rahul Rayal <rahul.rayal@D11-Rahul.Rayal>

---
+++ internal/constant/const.go
@@ -0,0 +1,4 @@
+package constant
+
+const ENV_NAME_OPTION string = "Environment name is not passed. Please pass it with --name option"
+const LABEL_NAME_OPTION string = "Label name is not passed. Please pass it with --name option"
