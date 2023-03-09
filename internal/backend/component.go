c7a4aa8bca831597143072a81e234b3291fda36e
rahulrayal <rahulrayal@dream11.com>
2023-03-09 21:50:30 +0530

    removed redeploy block from cli

--- internal/backend/component.go
+++ internal/backend/component.go
@@ -10,6 +10,7 @@ type Component struct{}
 
 func (c *Component) OperateComponent(componentName string, data component.OperateComponentRequest) {
 	client := newStreamingApiClient()
+	client.Headers["Command-Verb"] = "operate"
 	response := client.streamWithRetry(path.Join("component", componentName, "operate"), "PUT", data)
 	response.Process(true)
 }
