c738b26627154252b61181c431c1a058b8925252
rahulrayal <rahulrayal@dream11.com>
2023-03-17 21:14:45 +0530

    added consent for deploy service

--- internal/command/commands/service.go
+++ internal/command/commands/service.go
@@ -261,27 +261,6 @@ func (s *Service) Run(args []string) int {
 			return 1
 		}
 
-		envTypeResp, err := envTypeClient.GetEnvType(*envName)
-		if err != nil {
-			s.Logger.Error(err.Error())
-			return 1
-		}
-		if envTypeResp.Strict {
-			consentMessage := "\nYou are executing the above command on production environment. Are you sure? Enter Y/n:"
-			allowedInputs := map[string]struct{}{"Y": {}, "n": {}}
-			val, err := s.Input.AskWithConstraints(consentMessage, allowedInputs)
-
-			if err != nil {
-				s.Logger.Error(err.Error())
-				return 1
-			}
-
-			if val != "Y" {
-				s.Logger.Info("Aborting the operation")
-				return 1
-			}
-		}
-
 		emptyUnreleasedParameters := emptyParameters(map[string]string{"--env": *envName, "--file": *filePath})
 		if len(emptyUnreleasedParameters) == 0 {
 			parsedConfig, err := parseFile(*filePath)
@@ -289,12 +268,10 @@ func (s *Service) Run(args []string) int {
 				s.Logger.Error("Error while parsing service file " + *filePath + " : " + err.Error())
 				return 1
 			}
-
 			consent := s.askForConsent(envName)
 			if consent == 1 {
 				return 1
 			}
-
 			serviceDefinition := parsedConfig.(map[string]interface{})
 
 			return s.deployUnreleasedService(envName, serviceDefinition, provisioningConfigFile, configStoreNamespace)
