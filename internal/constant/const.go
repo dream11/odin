9edf2e3afaa8460954bd707e8047e8d4a2df71dc
Akshay Patidar <akshaypatidar1999@gmail.com>
2023-08-18 14:32:41 +0530

    chore: remove unwanted files
    Signed-off-by: Akshay Patidar <akshaypatidar1999@gmail.com>

--- internal/constant/const.go
+++
@@ -1,4 +0,0 @@
-package constant
-
-const ENV_NAME_OPTION string = "Environment name is not passed. Please pass it with --name option"
-const LABEL_NAME_OPTION string = "Label name is not passed. Please pass it with --name option"
