99afe648e3838113c019fa725546a304ef5c3f6d
surajgour-d11 <suraj.gour@dream11.com>
2024-04-23 18:39:12 +0530

    feat: add spinner in response
    Signed-off-by: surajgour-d11 <suraj.gour@dream11.com>

--- internal/service/environment.go
+++ internal/service/environment.go
@@ -2,13 +2,29 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"time"
 
+	"github.com/briandowns/spinner"
 	environment "github.com/dream11/odin/proto/gen/go/dream11/od/environment/v1"
 	log "github.com/sirupsen/logrus"
 )
 
 // Environment performs operation on environment like create, list, describe, delete
 type Environment struct{}
+
+// SpinnerColor Defines color of spinner
+var SpinnerColor = "fgHiBlue"
+
+// SpinnerStyle Defines style of spinner
+var SpinnerStyle = "bold"
+
+// SpinnerType Defines type of spinner
+var SpinnerType = 14
+
+// SpinnerDelay Defines spinner delay
+var SpinnerDelay = 100 * time.Millisecond
 
 // ListEnvironments List environments
 func (e *Environment) ListEnvironments(ctx *context.Context, request *environment.ListEnvironmentRequest) (*environment.ListEnvironmentResponse, error) {
@@ -38,17 +54,22 @@ func (e *Environment) CreateEnvironment(ctx *context.Context, request *environme
 	}
 
 	log.Info("Creating environment...")
+	spinner := spinner.New(spinner.CharSets[SpinnerType], SpinnerDelay)
+	spinner.Color(SpinnerColor, SpinnerStyle)
+
 	for {
 		response, err := stream.Recv()
+		spinner.Stop()
 		if err != nil {
-			if err == context.Canceled {
+			if err == context.Canceled || err == io.EOF {
 				break
 			}
 			return err
 		}
-
-		log.Info(response)
+		spinner.Prefix = fmt.Sprintf(" %s  ", response.Message)
+		spinner.Start()
 	}
+	log.Info("Environment created successfully")
 
 	return err
 }
