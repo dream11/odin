cfb02e070d8c430356c7ffaf345ee520b4d08133
Akshay Patidar <akshaypatidar1999@gmail.com>
2024-04-26 12:59:30 +0530

    fix: grpc connection for insecure (#226)
    Signed-off-by: Akshay Patidar <akshaypatidar1999@gmail.com>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,11 +1,20 @@
 package util
 
-import "strings"
+import (
+	"net"
+	"strings"
+)
 
-// SplitProviderAccount: splits string into list of cloud provider accounts
+// SplitProviderAccount splits string into list of cloud provider accounts
 func SplitProviderAccount(providerAccounts string) []string {
 	if providerAccounts == "" {
 		return nil
 	}
 	return strings.Split(providerAccounts, ",")
+}
+
+// IsIPAddress checks if given address is an IP address
+func IsIPAddress(address string) bool {
+	addr := net.ParseIP(address)
+	return addr != nil
 }
