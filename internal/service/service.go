e3d8680f3b6719d1e2cb3778968eed5af2006bfe
boppanasusanth <boppana.susanth@dream11.com>
2024-04-30 19:26:51 +0530

    onboard deploy service command

--- internal/service/service.go
+++ internal/service/service.go
@@ -15,7 +15,52 @@ import (
 // Service performs operation on service like deploy. undeploy
 type Service struct{}
 
-// UndeployService : Delete environment
+// DeployService deploys service
+func (e *Service) DeployService(ctx *context.Context, request *service.DeployServiceRequest) error {
+	conn, requestCtx, err := grpcClient(ctx)
+	if err != nil {
+		return err
+	}
+	client := service.NewServiceServiceClient(conn)
+	stream, err := client.DeployService(*requestCtx, request)
+	if err != nil {
+		return err
+	}
+
+	log.Info("Deploying Service...")
+	spinner := spinner.New(spinner.CharSets[constant.SpinnerType], constant.SpinnerDelay)
+	err = spinner.Color(constant.SpinnerColor, constant.SpinnerStyle)
+	if err != nil {
+		return err
+	}
+
+	var message string
+	for {
+		response, err := stream.Recv()
+		spinner.Stop()
+		if err != nil {
+			if errors.Is(err, context.Canceled) || err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		if response != nil {
+			message = response.Message
+			message += fmt.Sprintf("\n Service %s %s", response.ServiceStatus.ServiceAction, response.ServiceStatus.ServiceStatus)
+			for _, compMessage := range response.ComponentsStatus {
+				message += fmt.Sprintf("\n Component %s %s %s", compMessage.ComponentName, compMessage.ComponentAction, compMessage.ComponentStatus)
+			}
+			spinner.Prefix = fmt.Sprintf(" %s  ", message)
+			spinner.Start()
+		}
+	}
+
+	log.Info(message)
+	return err
+}
+
+// UndeployService undeploys service
 func (e *Service) UndeployService(ctx *context.Context, request *service.UndeployServiceRequest) error {
 	conn, requestCtx, err := grpcClient(ctx)
 	if err != nil {
