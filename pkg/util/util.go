ff9c076288a8c8d4324c5bc564cb4d7123088fb3
J Yash Sakariya Jain <yash.jain@dream11.com>
2024-04-30 16:17:59 +0530

    removed unwanted function

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"net"
-	"sort"
 	"strings"
 )
 
@@ -18,16 +17,4 @@ func SplitProviderAccount(providerAccounts string) []string {
 func IsIPAddress(address string) bool {
 	addr := net.ParseIP(address)
 	return addr != nil
-}
-
-// EmptyParameters get empty parameter list check if the map have any key with empty value if yes then return a comma seperated string of keys which have empty values
-func EmptyParameters(params map[string]string) string {
-	emptyParameters := []string{}
-	for key, val := range params {
-		if len(val) == 0 {
-			emptyParameters = append(emptyParameters, key)
-		}
-	}
-	sort.Strings(emptyParameters)
-	return strings.Join(emptyParameters, ", ")
 }
