f6332807d4587263b5c87afc4a7d556756d25cd9
J Yash Sakariya Jain <yash.jain@dream11.com>
2024-05-01 13:50:35 +0530

    added service operate cmd

--- internal/service/service.go
+++ internal/service/service.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/briandowns/spinner"
 	"github.com/dream11/odin/pkg/constant"
-	service "github.com/dream11/odin/proto/gen/go/dream11/od/service/v1"
+	serviceProto "github.com/dream11/odin/proto/gen/go/dream11/od/service/v1"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -16,13 +16,13 @@ import (
 type Service struct{}
 
 // UndeployService : Delete environment
-func (e *Service) UndeployService(ctx *context.Context, request *service.UndeployServiceRequest) error {
+func (e *Service) UndeployService(ctx *context.Context, request *serviceProto.UndeployServiceRequest) error {
 	conn, requestCtx, err := grpcClient(ctx)
 	if err != nil {
 		return err
 	}
 
-	client := service.NewServiceServiceClient(conn)
+	client := serviceProto.NewServiceServiceClient(conn)
 	stream, err := client.UndeployService(*requestCtx, request)
 
 	if err != nil {
@@ -30,15 +30,15 @@ func (e *Service) UndeployService(ctx *context.Context, request *service.Undeplo
 	}
 
 	log.Info("Undeploying Service...")
-	spinner := spinner.New(spinner.CharSets[constant.SpinnerType], constant.SpinnerDelay)
-	err = spinner.Color(constant.SpinnerColor, constant.SpinnerStyle)
+	spinnerInstance := spinner.New(spinner.CharSets[constant.SpinnerType], constant.SpinnerDelay)
+	err = spinnerInstance.Color(constant.SpinnerColor, constant.SpinnerStyle)
 	if err != nil {
 		return err
 	}
 	var message string
 	for {
 		response, err := stream.Recv()
-		spinner.Stop()
+		spinnerInstance.Stop()
 		if err != nil {
 			if errors.Is(err, context.Canceled) || err == io.EOF {
 				break
@@ -47,8 +47,46 @@ func (e *Service) UndeployService(ctx *context.Context, request *service.Undeplo
 		}
 		if response != nil {
 			message = response.Message
-			spinner.Prefix = fmt.Sprintf(" %s  ", response.Message)
-			spinner.Start()
+			spinnerInstance.Prefix = fmt.Sprintf(" %s  ", response.Message)
+			spinnerInstance.Start()
+		}
+	}
+	log.Info(message)
+	return err
+}
+
+// OperateService :service operatioms
+func (e *Service) OperateService(ctx *context.Context, request *serviceProto.OperateServiceRequest) error {
+	conn, requestCtx, err := grpcClient(ctx)
+	if err != nil {
+		return err
+	}
+	client := serviceProto.NewServiceServiceClient(conn)
+	stream, err := client.OperateService(*requestCtx, request)
+	if err != nil {
+		return err
+	}
+
+	log.Info("Starting component operation...")
+	spinnerInstance := spinner.New(spinner.CharSets[constant.SpinnerType], constant.SpinnerDelay)
+	err = spinnerInstance.Color(constant.SpinnerColor, constant.SpinnerStyle)
+	if err != nil {
+		return err
+	}
+	var message string
+	for {
+		response, err := stream.Recv()
+		spinnerInstance.Stop()
+		if err != nil {
+			if errors.Is(err, context.Canceled) || err == io.EOF {
+				break
+			}
+			return err
+		}
+		if response != nil {
+			message = response.Message
+			spinnerInstance.Prefix = fmt.Sprintf(" %s  ", response.Message)
+			spinnerInstance.Start()
 		}
 	}
 	log.Info(message)
