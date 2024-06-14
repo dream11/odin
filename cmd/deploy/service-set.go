2280c6adf2946d64d53c540732161cbef2ad05b4
J Yash Sakariya Jain <113010737+yashjaind11@users.noreply.github.com>
2024-06-14 15:05:55 +0530

    Feat/deploy serviceset (#241)
    * deployed service proto update
    
    * deploye service set cmd implementation
    
    * name correction
    
    * lint changes
    
    * pr changes
    
    * pr changes

---
+++ cmd/deploy/service-set.go
@@ -0,0 +1,71 @@
+package deploy
+
+import (
+	"encoding/json"
+	"os"
+
+	serviceDto "github.com/dream11/odin/proto/gen/go/dream11/od/dto/v1"
+	serviceProto "github.com/dream11/odin/proto/gen/go/dream11/od/service/v1"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var serviceSetName string
+var serviceSetDeployCmd = &cobra.Command{
+	Use:   "service-set",
+	Short: "Deploy service-set",
+	Args: func(cmd *cobra.Command, args []string) error {
+		return cobra.NoArgs(cmd, args)
+	},
+	Long: "Deploy service-set using files or service set name",
+	Run: func(cmd *cobra.Command, args []string) {
+		executeDeployServiceSet(cmd)
+	},
+}
+
+func init() {
+
+	serviceSetDeployCmd.Flags().StringVar(&env, "env", "", "environment for deploying the service-set")
+	serviceSetDeployCmd.Flags().StringVar(&provisioningFile, "file", "", "path to the service set provisioning file")
+	serviceSetDeployCmd.Flags().StringVar(&serviceSetName, "name", "", "released service set name")
+
+	err := serviceSetDeployCmd.MarkFlagRequired("env")
+	if err != nil {
+		log.Fatal("Error marking 'env' flag as required:", err)
+	}
+
+	deployCmd.AddCommand(serviceSetDeployCmd)
+}
+
+func executeDeployServiceSet(cmd *cobra.Command) {
+	ctx := cmd.Context()
+	if serviceSetName == "" && provisioningFile == "" {
+		log.Fatal("Please provide either --name or --file.")
+	}
+	if provisioningFile != "" && serviceSetName != "" {
+		log.Fatal("--name should not be provided when --file is provided.")
+	}
+	var deployServiceSetRequestDTO serviceDto.ServiceSet
+	var deployServiceSetRequest serviceProto.DeployServiceSetRequest
+
+	deployServiceSetRequest.EnvName = env
+
+	if provisioningFile != "" {
+		provisioningData, err := os.ReadFile(provisioningFile)
+		if err != nil {
+			log.Fatal("Error while reading provisioning file ", err)
+		}
+		if err := json.Unmarshal(provisioningData, &deployServiceSetRequestDTO); err != nil {
+			log.Fatal("error unmarshalling provisioning file: %w", err)
+		}
+		deployServiceSetRequest = serviceClient.ConvertToDeployServiceSetRequest(&deployServiceSetRequestDTO, env)
+	}
+	if serviceSetName != "" {
+		deployServiceSetRequest.Name = serviceSetName
+	}
+
+	err := serviceClient.DeployServiceSet(&ctx, &deployServiceSetRequest)
+	if err != nil {
+		log.Fatal("Failed to deploy service ", err)
+	}
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/briandowns/spinner"
 	"github.com/dream11/odin/pkg/constant"
+	serviceDto "github.com/dream11/odin/proto/gen/go/dream11/od/dto/v1"
 	serviceProto "github.com/dream11/odin/proto/gen/go/dream11/od/service/v1"
 	log "github.com/sirupsen/logrus"
 )
@@ -60,6 +61,53 @@ func (e *Service) DeployService(ctx *context.Context, request *serviceProto.Depl
 	return err
 }
 
+// DeployServiceSet deploys service-set
+func (e *Service) DeployServiceSet(ctx *context.Context, request *serviceProto.DeployServiceSetRequest) error {
+	conn, requestCtx, err := grpcClient(ctx)
+	if err != nil {
+		return err
+	}
+	client := serviceProto.NewServiceServiceClient(conn)
+	stream, err := client.DeployServiceSet(*requestCtx, request)
+	if err != nil {
+		return err
+	}
+
+	log.Info("Deploying Service Set..")
+	spinnerInstance := spinner.New(spinner.CharSets[constant.SpinnerType], constant.SpinnerDelay)
+	err = spinnerInstance.Color(constant.SpinnerColor, constant.SpinnerStyle)
+	if err != nil {
+		return err
+	}
+
+	var message string
+	for {
+		response, err := stream.Recv()
+		spinnerInstance.Stop()
+		if err != nil {
+			if errors.Is(err, context.Canceled) || err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		if response != nil {
+
+			for _, serviceRespose := range response.GetServices() {
+				message = fmt.Sprintf("\n Service %s %s %s", serviceRespose.ServiceIdentifier, serviceRespose.ServiceResponse.ServiceStatus, serviceRespose.ServiceResponse.Message)
+				for _, compMessage := range serviceRespose.ServiceResponse.ComponentsStatus {
+					message += fmt.Sprintf("\n Component %s %s %s", compMessage.ComponentName, compMessage.ComponentAction, compMessage.ComponentStatus)
+				}
+			}
+			spinnerInstance.Prefix = fmt.Sprintf(" %s  ", message)
+			spinnerInstance.Start()
+		}
+	}
+
+	log.Info(message)
+	return err
+}
+
 // DeployReleasedService deploys service
 func (e *Service) DeployReleasedService(ctx *context.Context, request *serviceProto.DeployReleasedServiceRequest) error {
 	conn, requestCtx, err := grpcClient(ctx)
@@ -91,9 +139,9 @@ func (e *Service) DeployReleasedService(ctx *context.Context, request *servicePr
 		}
 
 		if response != nil {
-			message = response.Message
-			message += fmt.Sprintf("\n Service %s %s", response.ServiceStatus.ServiceAction, response.ServiceStatus.ServiceStatus)
-			for _, compMessage := range response.ComponentsStatus {
+			message = response.GetServiceResponse().Message
+			message += fmt.Sprintf("\n Service %s %s", response.GetServiceResponse().ServiceStatus.ServiceAction, response.GetServiceResponse().ServiceStatus.ServiceStatus)
+			for _, compMessage := range response.GetServiceResponse().ComponentsStatus {
 				message += fmt.Sprintf("\n Component %s %s %s", compMessage.ComponentName, compMessage.ComponentAction, compMessage.ComponentStatus)
 			}
 			spinnerInstance.Prefix = fmt.Sprintf(" %s  ", message)
@@ -200,4 +248,21 @@ func (e *Service) ListService(ctx *context.Context, request *serviceProto.ListSe
 	client := serviceProto.NewServiceServiceClient(conn)
 	response, err := client.ListService(*requestCtx, request)
 	return response, err
+}
+
+// ConvertToDeployServiceSetRequest converts service set to deploy service set request
+func (e *Service) ConvertToDeployServiceSetRequest(serviceSet *serviceDto.ServiceSet, env string) serviceProto.DeployServiceSetRequest {
+	var services []*serviceProto.ServiceIdentifier
+	for _, service := range serviceSet.Services {
+		services = append(services, &serviceProto.ServiceIdentifier{
+			ServiceName:    service.Name,
+			ServiceVersion: service.Version,
+		})
+	}
+
+	return serviceProto.DeployServiceSetRequest{
+		EnvName:  env,
+		Name:     serviceSet.Name,
+		Services: services,
+	}
 }
