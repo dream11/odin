5eca201c885473f178dcf483be55e2193b3acd7d
surajgour-d11 <suraj.gour@dream11.com>
2024-12-25 12:23:49 +0530

    feat: enable debug level logs support
    Signed-off-by: surajgour-d11 <suraj.gour@dream11.com>

--- internal/ui/logger.go
+++ internal/ui/logger.go
@@ -1,12 +1,22 @@
 package ui
 
 import (
+	"github.com/dream11/odin/pkg/constant"
 	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 func init() {
+
 	log.SetFormatter(&log.TextFormatter{
-		ForceColors:   true,
-		DisableColors: false,
+		ForceColors:     true,
+		DisableColors:   false,
+		TimestampFormat: "2006-01-02 15:04:05", // Custom format
+		FullTimestamp:   true,
 	})
+	if os.Getenv(constant.LogLevelKey) == "yes" {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
 }
