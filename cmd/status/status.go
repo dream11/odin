ebf049613bbc444b38d93f9d55624b17fde81f00
aakash-d11 <aakash.tiwari@dream11.com>
2025-03-02 17:17:16 +0530

    chore: fixed the error log for set env (#294)
    * chore: fixed the error log for set env
    
    * chore:  updated the help messages for update, status and operate
    
    * chore:  added comments for golint checks

--- cmd/status/status.go
+++ cmd/status/status.go
@@ -8,8 +8,11 @@ import (
 // statusCmd represents the status command
 var statusCmd = &cobra.Command{
 	Use:   "status",
-	Short: "This command is accessed by using one of the subcommands below.",
-	Long:  `This command is accessed by using one of the subcommands below.`,
+	Short: "This command is accessed by using one of the subcommands: [env]",
+	Long: `This command is accessed by using one of the subcommands below:
+
+Subcommands:
+    env          Fetch deployment status of the environment`,
 }
 
 func init() {
