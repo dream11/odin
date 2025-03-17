9ea3caf1296c20491b8aae81297e9ad82ae741d8
Aakash Tiwari <aakash.tiwari@dream11.com>
2025-03-17 21:22:51 +0530

    updated script

--- switch.py
+++ switch.py
@@ -473,10 +473,12 @@ def main():
             else:
                 if "--file" in sys.argv:
                     create_new_service_set_and_trigger_odin(sys.argv[sys.argv.index("--file") + 1])
-                else:
-                    print("Command deprecated, use file to deploy. Refer to the service set file on https://github.com/dream11/service-sets to "
-                  "learn more about it")
+                elif "--name" in sys.argv:
+                    print("--name flag is deprecated and no longer supported. Please use --file flag to deploy your service-set.To get service set definitions"
+                  " please refer https://github.com/dream11/service-sets")
                     exit(0)
+                else:
+                    execute_new_odin()
         else:
             execute_new_odin()
 
