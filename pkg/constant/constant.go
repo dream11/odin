81bc4d24590616c0c48d4a1a830545cbcfedda8e
Suraj Gour <85784713+surajgour-d11@users.noreply.github.com>
2025-04-17 19:16:11 +0530

    Feat/logs operate (#325)
    * Chore/refractor livelogs (#322)
    
    * chore: refractoring
    
    Signed-off-by: surajgour-d11 <suraj.gour@dream11.com>
    
    * chore: refractoring
    
    Signed-off-by: surajgour-d11 <suraj.gour@dream11.com>
    
    ---------
    
    Signed-off-by: surajgour-d11 <suraj.gour@dream11.com>
    
    * chore: added comments
    
    Signed-off-by: surajgour-d11 <suraj.gour@dream11.com>
    
    * feat: generic response handler
    
    Signed-off-by: surajgour-d11 <suraj.gour@dream11.com>
    
    ---------
    
    Signed-off-by: surajgour-d11 <suraj.gour@dream11.com>

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -34,10 +34,10 @@ const (
 	InitiatingRetryMessage = "Unable to reach Odin backend."
 	// MaxRetries is the maximum number of retries
 	MaxRetries = 5
-	// Timeout is the timeout duration for requests
-	Timeout = 10 * time.Second
+	// Delay is the delay between retries
+	Delay = 10 * time.Second
 	// MaxRetriesReached is the message shown when max retries are reached
 	MaxRetriesReached = "Max retries reached. Exiting...\nPlease check:\n- Your internet connection: \n- VPN connected properly"
-	DescribeEnv       = "\n- Please use describe env command to check the status.\n \u001B[1m odin describe env --name %s\u001B[0m  "
-	RetryingMessage   = "Retrying ... (%d/%d)"
+	// RetryMessage is the message shown when retrying
+	RetryingMessage = "Retrying ... (%d/%d)"
 )
