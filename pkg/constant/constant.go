1bda9d7ef0d78ac6300b96f16fddb5f63c73d571
mnishhhD11 <manish.patel@dream11.com>
2025-05-13 15:25:40 +0530

    Feat/log enhancement (#331)
    * add debug level to logs with no level
    
    * verbose changes
    
    * sort imports
    
    * resolve comments

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -5,6 +5,9 @@ import "time"
 // TraceID is the type for trace ID
 type TraceID string
 
+// VerboseEnabled is the type for verboseEnabledKey
+type VerboseEnabled string
+
 const (
 	// TEXT type output format
 	TEXT = "text"
@@ -24,6 +27,12 @@ const (
 
 	// TraceIDKey is the key used to store traceID in context
 	TraceIDKey TraceID = "trace-id"
+
+	// VerboseEnabledKey is the key used to store verbose value in context
+	VerboseEnabledKey VerboseEnabled = "verbose"
+
+	// VerboseFlag is the key used to store verbose value
+	VerboseFlag string = "verbose"
 
 	// LogLevelKey is the key used to set log level
 	LogLevelKey = "ODIN_LOG_LEVEL"
