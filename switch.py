ed7f71e76f375c3eebbe88916d0dafc98b8dcc53
umanggoyald11 <122361006+umanggoyald11@users.noreply.github.com>
2025-12-10 18:45:55 +0530

    Update/mig umag (#360)
    * add sandbox in prod env list
    
    * Add d11 prod
    
    * Fix: Add uat
    
    * CLI code-artifact changes (removed lables) (#358)
    
    * removed version to stable conversion
    
    ---------
    
    Co-authored-by: satyam-d11 <satyam.chaurasiya@dream11.com>

--- switch.py
+++ switch.py
@@ -376,23 +376,6 @@ def transform_service_set_file(content):
         print("Error: JSON file must contain a 'services' array")
         sys.exit(1)
 
-    # Transform each service in the array
-    for service in data['services']:
-        if isinstance(service, dict) and 'version' in service:
-            version = service['version']
-
-            if version == 'stable':
-                del service['version']
-                service['labels'] = 'isStable=true'
-
-            elif version == 'dev-stable':
-                del service['version']
-                service['labels'] = 'isDevStable=true'
-
-            elif version == 'load-stable':
-                del service['version']
-                service['labels'] = 'isLoadStable=true'
-
     return json.dumps(data, indent=2)
 
 
@@ -413,16 +396,6 @@ def create_new_service_set_and_trigger_odin(original_file):
         file_index = updated_args.index("--file") + 1
         updated_args[file_index] = new_filename
         execute_new_odin_with_custom_cmd(updated_args)
-
-def get_label_from_version(version):
-    if version == 'stable':
-        return 'isStable=true'
-    elif version == 'dev-stable':
-        return 'isDevStable=true'
-    elif version == 'load-stable':
-        return 'isLoadStable=true'
-    else:
-        return None
 
 def main():
     global odin_access_key, odin_secret_access_key, odin_access_token, odin_backend_address, OLD_ODIN
@@ -518,7 +491,7 @@ def main():
                 account_index = sys.argv.index("--accounts")
                 provided_accounts_str = sys.argv[account_index + 1]
                 accounts = set(provided_accounts_str.split(","))
-                if "stag" in provided_accounts_str:
+                if "staging" in provided_accounts_str:
                     accounts.add("staging")
                     accounts.add("staging_gcp")
                 elif "load" in provided_accounts_str:
@@ -597,7 +570,7 @@ def main():
         if env_name is not None and check_env_exists_in_old_odin(env_name):
             if service_name is not None and is_service_migrated_to_new_odin(service_name, env_name):
                 execute_new_odin()
-            elif service_name is not None and not does_service_exist_in_old_odin_env(service_name, env_name) and env_name in ["prod", "auth-bom", "auth-nv", "uat", "sandbox", "hulk-uat1", "hulk-prod","hulkprod","obs-prod","d11-prod"]:
+            elif service_name is not None and not does_service_exist_in_old_odin_env(service_name, env_name) and env_name in ["prod", "auth-bom", "auth-nv", "uat", "sandbox", "hulk-uat1", "hulk-prod","hulkprod","obs-prod","d11prod","d11uat"]:
                 execute_new_odin()
             else:
                 execute_old_odin()
@@ -605,11 +578,7 @@ def main():
             if service_name is not None:
                 if "--version" in sys.argv:
                     version = sys.argv[sys.argv.index("--version") + 1]
-                    label = get_label_from_version(version)
-                    if label is not None:
-                        sys.argv[sys.argv.index("--version") + 1] = label
-                        sys.argv[sys.argv.index("--version")] = "--labels"
-            execute_new_odin()    
+            execute_new_odin()
     else:
         execute_new_odin()
 
